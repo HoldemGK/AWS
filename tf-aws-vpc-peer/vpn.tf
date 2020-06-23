d5f76f6a81c983458b6e56062ff94cc76b11ccf5
Sanchez <skrolsky@gmail.com>
2020-06-23 14:56:01 +0300

    Update vpn.tf

--- tf-aws-vpc-peer/vpn.tf
+++ tf-aws-vpc-peer/vpn.tf
@@ -10,7 +10,7 @@ resource "aws_vpn_gateway" "aws_vpn_gateway_1" {
 }
 
 resource "aws_customer_gateway" "customer_gateway_1" {
-  static_routes_only = true
+  bgp_asn    = 65000
   ip_address = "199.126.129.250"
   type       = "ipsec.1"
 }
@@ -19,6 +19,7 @@ resource "aws_vpn_connection" "main" {
   vpn_gateway_id      = aws_vpn_gateway.aws_vpn_gateway_1.id
   customer_gateway_id = aws_customer_gateway.customer_gateway_1.id
   type                = "ipsec.1"
+  static_routes_only = true
   tags = {
     Name = "east_vpn_1"
   }
