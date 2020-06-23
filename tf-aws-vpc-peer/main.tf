0f43944543875911c1a25c63ad0ef79749c7480e
Sanchez <skrolsky@gmail.com>
2020-06-23 14:53:31 +0300

    add vpn

--- tf-aws-vpc-peer/main.tf
+++ tf-aws-vpc-peer/main.tf
@@ -1,25 +1,3 @@
 provider "aws" {
   region = var.region
 }
-
-resource "aws_vpc" "bar" {
-  cidr_block = "12.0.0.0/16"
-
-  tags = {
-    Name = "bar"
-  }
-}
-
-resource "aws_vpc" "foo" {
-  cidr_block = "11.0.0.0/16"
-
-  tags = {
-    Name = "foo"
-  }
-}
-
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
-  peer_region   = var.region
-}
---
+++ tf-aws-vpc-peer/vpn.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "15.0.0.0/16"
+  tags = {
+    Name = "LAB"
+  }
+}
+
+resource "aws_vpn_gateway" "aws_vpn_gateway_1" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_customer_gateway" "customer_gateway_1" {
+  static_routes_only = true
+  ip_address = "199.126.129.250"
+  type       = "ipsec.1"
+}
+
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id      = aws_vpn_gateway.aws_vpn_gateway_1.id
+  customer_gateway_id = aws_customer_gateway.customer_gateway_1.id
+  type                = "ipsec.1"
+  tags = {
+    Name = "east_vpn_1"
+  }
+}
