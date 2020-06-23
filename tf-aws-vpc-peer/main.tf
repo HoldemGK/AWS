adfb58d5ee3c68ee700d323f6a3e3443f8cd843c
Sanchez <skrolsky@gmail.com>
2020-06-23 09:39:34 +0300

    Update main.tf

--- tf-aws-vpc-peer/main.tf
+++ tf-aws-vpc-peer/main.tf
@@ -2,15 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
-  peer_region   = var.region
-}
-
 resource "aws_vpc" "foo" {
   provider   = "aws.us-west-2"
-  cidr_block = "10.1.0.0/16"
+  cidr_block = "11.0.0.0/16"
+
+  tags = {
+    Name = "foo"
+  }
+}
+
+resource "aws_subnet" "foo_sub" {
+  vpc_id     = aws_vpc.foo.id
+  cidr_block = "11.0.1.0/24"
 
   tags = {
     Name = "foo"
@@ -19,19 +22,36 @@ resource "aws_vpc" "foo" {
 
 resource "aws_vpc" "bar" {
   provider   = "aws.us-east-1"
-  cidr_block = "10.2.0.0/16"
+  cidr_block = "12.0.0.0/16"
 
   tags = {
     Name = "bar"
   }
 }
 
+resource "aws_subnet" "bar_sub" {
+  vpc_id     = aws_vpc.bar.id
+  cidr_block = "12.0.1.0/24"
+
+  tags = {
+    Name = "bar"
+  }
+}
+
+resource "aws_vpc_peering_connection" "foo" {
+  peer_vpc_id   = aws_vpc.bar.id
+  vpc_id        = aws_vpc.foo.id
+  peer_region   = var.region
+}
+
 resource "aws_instance" "foo_inst" {
   ami = var.ami-west
   instance_type = var.instance_type
+  subnet_id = aws_subnet.foo_sub.id
 }
 
 resource "aws_instance" "bar_inst" {
   ami = var.ami-east
   instance_type = var.instance_type
+  subnet_id = aws_subnet.bar_sub.id
 }
