97c9383f6ef1814395476c1277b531d91ab7197f
Sanchez <atos.holdemgk@gmail.com>
2021-10-03 22:11:44 +0300

    Update bastion.tf

--- tf-k3s/bastion.tf
+++ tf-k3s/bastion.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "bastion" {
   name = var.env_name
   ami = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.subnets.id
+  subnet_id = aws_default_subnet.ap_subnet.id
   key_pair_name = var.key_pair_name
   security_groups = [aws_security_group.bastion.id]
 }
