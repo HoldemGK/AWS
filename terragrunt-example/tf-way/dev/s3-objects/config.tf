1e5487c01fa843d6f4e1de7f2930f1910eabe6e1
Aleksandr Vasilev3 <aleksandr_vasilev3@epam.com>
2022-04-09 20:24:23 +0200

    init terragrunt

---
+++ terragrunt-example/tf-way/dev/s3-objects/config.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+      bucket = "terraform-backend-gk"
+      key = "dev/s3-objects/terraform.tfstate"
+      region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = data.vault_generic_secret.aws_creds.data["aws_access_key_id"]
+  secret_key = data.vault_generic_secret.aws_creds.data["aws_secret_access_key"]
+}
+
+data "vault_generic_secret" "aws_creds" {
+  path = "secret/aws"
+}
