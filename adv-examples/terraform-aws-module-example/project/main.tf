d27c39b99ff7b0580737eb2585eae6381e054790
Aleksandr Vasilev3 <aleksandr_vasilev3@epam.com>
2022-04-24 14:55:41 +0200

    remote modules

--- adv-examples/terraform-aws-module-example/project/main.tf
+++ adv-examples/terraform-aws-module-example/project/main.tf
@@ -1,9 +1,10 @@
 # module "vpc-default" {
-#   source = "../modules/aws_network"
+#   source = "git@github.com:HoldemGK/AWS.git//adv-examples/terraform-aws-module-example/modules/aws_network"
 # }
 
 module "vpc-dev" {
-  source               = "../modules/aws_network"
+  #source               = "../modules/aws_network"
+  source               = "git@github.com:HoldemGK/AWS.git//adv-examples/terraform-aws-module-example/modules/aws_network"
   env                  = "development"
   vpc_cidr             = "10.100.0.0/16"
   public_subnet_cidrs  = ["10.100.1.0/24", "10.100.2.0/24"]
@@ -11,7 +12,7 @@ module "vpc-dev" {
 }
 
 module "vpc-prod" {
-  source               = "../modules/aws_network"
+  source               = "git@github.com:HoldemGK/AWS.git//adv-examples/terraform-aws-module-example/modules/aws_network"
   env                  = "production"
   vpc_cidr             = "10.10.0.0/16"
   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
