0bd2b5d165114196d765ab45c08c1217b1285b85
Aleksandr Vasilev3 <aleksandr_vasilev3@epam.com>
2022-04-24 13:17:18 +0200

    add module folders

--- adv-examples/terraform-aws-module-example/config.tf
+++ adv-examples/terraform-aws-module-example/config.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-      bucket = "terraform-backend-gk"
-      key = "dev/s3/terraform.tfstate"
-      region = "eu-north-1"
+    bucket = "terraform-backend-gk"
+    key    = "terraform-aws-module/terraform.tfstate"
+    region = "eu-north-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = data.vault_generic_secret.aws_creds.data["aws_access_key_id"]
   secret_key = data.vault_generic_secret.aws_creds.data["aws_secret_access_key"]
 }
--- adv-examples/terraform-aws-module-example/modules/aws_network/variables.tf
+++ adv-examples/terraform-aws-module-example/modules/aws_network/variables.tf
@@ -9,13 +9,15 @@ variable "env" {
 variable "public_subnet_cidrs" {
   default = [
     "10.0.1.0/24",
-    "10.0.2.0/24"
+    "10.0.2.0/24",
+    "10.0.3.0/24"
   ]
 }
 
 variable "private_subnet_cidrs" {
   default = [
-    "10.0.11.0/24",
-    "10.0.22.0/24"
+#    "10.0.11.0/24",
+#    "10.0.22.0/24",
+#    "10.0.33.0/24"
   ]
 }
