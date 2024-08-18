0fcc5c1b7f96c8d989f517ba8bb7d9210100d622
HoldemGK <atos.holdemgk@gmail.com>
2024-08-18 16:47:41 +0200

    added out adn disk

--- tf-k8s-kubeadm/main.tf
+++ tf-k8s-kubeadm/main.tf
@@ -23,12 +23,17 @@ module "ec2_master" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = local.master_name
-
   instance_type          = var.master_type
   key_name               = var.key_name
-  vpc_security_group_ids = ["sg-02357d42543ee294d"]
   subnet_id              = var.subnet_id
   ami                    = data.aws_ami.ubuntu.id
+
+  root_block_device = {
+        delete_on_termination = true
+        volume_size           = 30
+  }
+
+  vpc_security_group_ids = ["sg-02357d42543ee294d"]
 
   tags = {
     Name = local.master_name
