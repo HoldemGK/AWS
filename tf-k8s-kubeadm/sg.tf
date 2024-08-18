a59e6f4464bf4ba740ac34c377b521bbbc2346f3
HoldemGK <atos.holdemgk@gmail.com>
2024-08-18 22:31:14 +0200

    debug SG

--- tf-k8s-kubeadm/sg.tf
+++ tf-k8s-kubeadm/sg.tf
@@ -6,7 +6,7 @@ module "bastion_sg" {
   description = "Security group for Bastion host"
   vpc_id      = var.vpc_id
 
-  ingress_cidr_blocks = ["172.31.0.0/16"] # Allow SSH only from VPC_ID
+  ingress_cidr_blocks = ["172.31.0.0/16"] # Allow SSH only from your admin IP
   ingress_rules       = ["ssh"]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
@@ -25,21 +25,6 @@ module "k8s_master_sg" {
     "ssh",  # SSH access from Bastion
   ]
 
-  ingress_with_source_security_group_id = [
-    {
-      rule                     = "ssh"
-      source_security_group_id = module.bastion_sg.this_security_group_id
-    },
-    {
-      rule                     = "custom"
-      protocol                 = "tcp"
-      from_port                = 6443
-      to_port                  = 6443
-      description              = "Allow Kubernetes API server traffic from workers"
-      source_security_group_id = module.k8s_worker_sg.this_security_group_id
-    },
-  ]
-
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
 }
@@ -56,29 +41,36 @@ module "k8s_worker_sg" {
     "ssh",  # SSH access from Bastion
   ]
 
-  ingress_with_source_security_group_id = [
-    {
-      rule                     = "ssh"
-      source_security_group_id = module.bastion_sg.this_security_group_id
-    },
-    {
-      rule                     = "custom"
-      protocol                 = "tcp"
-      from_port                = 10250
-      to_port                  = 10250
-      description              = "Allow kubelet API traffic from master"
-      source_security_group_id = module.k8s_master_sg.this_security_group_id
-    },
-    {
-      rule                     = "custom"
-      protocol                 = "tcp"
-      from_port                = 30000
-      to_port                  = 32767
-      description              = "Allow Kubernetes NodePort traffic"
-      source_security_group_id = module.k8s_master_sg.this_security_group_id
-    },
-  ]
-
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
+}
+
+resource "aws_security_group_rule" "master_allow_worker" {
+  type                     = "ingress"
+  from_port                = 6443
+  to_port                  = 6443
+  protocol                 = "tcp"
+  security_group_id        = module.k8s_master_sg.this_security_group_id
+  source_security_group_id = module.k8s_worker_sg.this_security_group_id
+  description              = "Allow Kubernetes API server traffic from workers"
+}
+
+resource "aws_security_group_rule" "worker_allow_master" {
+  type                     = "ingress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = module.k8s_worker_sg.this_security_group_id
+  source_security_group_id = module.k8s_master_sg.this_security_group_id
+  description              = "Allow kubelet API traffic from master"
+}
+
+resource "aws_security_group_rule" "worker_allow_nodeport" {
+  type                     = "ingress"
+  from_port                = 30000
+  to_port                  = 32767
+  protocol                 = "tcp"
+  security_group_id        = module.k8s_worker_sg.this_security_group_id
+  source_security_group_id = module.k8s_master_sg.this_security_group_id
+  description              = "Allow Kubernetes NodePort traffic"
 }
