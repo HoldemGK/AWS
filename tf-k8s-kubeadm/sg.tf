906e16294b9fc9ba0912c70e3acfb57d5a2b4824
HoldemGK <atos.holdemgk@gmail.com>
2024-08-19 22:17:32 +0200

    debug sg

--- tf-k8s-kubeadm/sg.tf
+++ tf-k8s-kubeadm/sg.tf
@@ -1,6 +1,6 @@
 module "bastion_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
+  version = "5.1.2"
 
   name        = "bastion-sg"
   description = "Security group for Bastion host"
@@ -17,7 +17,7 @@ module "bastion_sg" {
 
 module "k8s_master_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
+  version = "5.1.2"
 
   name        = "k8s-master-sg"
   description = "Security group for Kubernetes Master nodes"
@@ -32,15 +32,13 @@ module "k8s_master_sg" {
 
 module "k8s_worker_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
+  version = "5.1.2"
 
   name        = "k8s-worker-sg"
   description = "Security group for Kubernetes Worker nodes"
   vpc_id      = var.vpc_id
 
-  ingress_rules = ["kubernetes-api"]
-  egress_rules  = local.worker_egress_rules
-
-  egress_cidr_blocks = ["0.0.0.0/0"]
-  #rules              = local.rules
+  ingress_cidr_blocks = ["172.31.0.0/16"]
+  ingress_rules       = ["all-icmp", "ssh-tcp", "kubernetes-api"]
+  egress_rules        = ["all-all"]
 }
