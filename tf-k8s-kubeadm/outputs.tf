6c87322451df653238c0ff18bbe04a33b48ac03e
HoldemGK <atos.holdemgk@gmail.com>
2024-08-19 22:20:07 +0200

    debug

--- tf-k8s-kubeadm/outputs.tf
+++ tf-k8s-kubeadm/outputs.tf
@@ -2,6 +2,6 @@ output "master_private_ip" {
   value = module.ec2_master.private_ip
 }
 
-output "worker_security_group_id" {
-  value = module.k8s_worker_sg.security_group_id
-}
+# output "worker_security_group_id" {
+#   value = module.k8s_worker_sg.security_group_id
+# }
